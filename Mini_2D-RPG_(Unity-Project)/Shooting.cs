91ef44b3be6998bd7df491dec44452f936c8cc6e
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2021-01-07 11:42:34 +0100

    gamr-folder added

---
+++ Mini_2D-RPG_(Unity-Project)/Shooting.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform FirePoint;
+    public Transform FirePointLeft;
+    public Transform FirePointUp;
+    public Transform FirePointDown;
+
+    public GameObject arrowPrefab;
+    public GameObject arrowLeftPrefab;
+    public GameObject arrowUpPrefab;
+    public GameObject arrowDownPrefab;
+
+    public Animator animator;
+
+    public float arrowSpeed = 30f;
+    private Vector2 direction;
+    bool canAttack = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J) && canAttack)
+        {
+                StartCoroutine(ShootCo());
+        }
+    }
+
+    private IEnumerator ShootCo()
+    {
+        canAttack = false;
+        if(Input.GetAxisRaw("Horizontal") == 1 || animator.GetFloat("LastHorizontal") > 0.01)
+        {
+            yield return new WaitForSeconds(0.3f);
+            ShootRight();
+            
+        }
+        else if(Input.GetAxisRaw("Horizontal") == -1 || animator.GetFloat("LastHorizontal") < 0)
+        {
+            yield return new WaitForSeconds(0.3f);
+            ShootLeft();
+        }
+        else if(Input.GetAxisRaw("Vertical") == 1 || animator.GetFloat("LastVertical") > 0.01)
+        {
+            yield return new WaitForSeconds(0.3f);
+            ShootUp();
+        }
+        else if(Input.GetAxisRaw("Vertical") == -1 || animator.GetFloat("LastVertical") < 0)
+        {
+            yield return new WaitForSeconds(0.3f);
+            ShootDown();
+        }
+        canAttack = true;
+    }
+
+    void ShootRight()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, FirePoint.position, FirePoint.rotation); //Spawnt den Pfeil am FirePoint
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>(); // Ein Rigidbody mit Name "rb" wird erstellt, welcher gleich mit dem des arrows ist
+        rb.AddForce(new Vector2 (arrowSpeed, 0), ForceMode2D.Impulse); // Pfeil mit arrowSpeed nach rechts fliegen lassen
+
+    }
+
+    void ShootLeft()
+    {
+        GameObject arrowLeft = Instantiate(arrowLeftPrefab, FirePointLeft.position, FirePointLeft.rotation);
+        Rigidbody2D rb = arrowLeft.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2 (-arrowSpeed, 0), ForceMode2D.Impulse);
+    }
+
+    void ShootUp()
+    {
+        GameObject arrowUp = Instantiate(arrowUpPrefab, FirePointUp.position, FirePointUp.rotation);
+        Rigidbody2D rb = arrowUp.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2 (0, arrowSpeed), ForceMode2D.Impulse);
+    }
+
+    void ShootDown()
+    {
+        GameObject arrowDown = Instantiate(arrowDownPrefab, FirePointDown.position, FirePointDown.rotation);
+        Rigidbody2D rb = arrowDown.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2 (0, -arrowSpeed), ForceMode2D.Impulse);
+    }
+}
---
+++ Mini_2D-RPG_(Unity-Project)/mole1.cs
@@ -0,0 +1,156 @@
+﻿//Tutorial Part 17 weiter
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mole1 : enemy
+{
+    public GameObject Enemy1;
+    public Rigidbody2D rigidEnemy;
+    public Animator animator;
+    public Animator playerAnimator;
+    int currentHealth;
+    public int playerHealth = 100;
+    private bool canAttack = true;
+    private bool enemyRage = false;
+
+    //movement:
+    public Transform target;
+    public float detectRadius;
+    public float stopRadius;
+    public float attackRadius;
+
+    /*knockback:
+    public float impact;
+    public float knockTime;
+    private Vector2 difference; */
+
+    void Start() 
+    {
+        currentHealth = maxHealth;
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        checkDistance();
+        controlAttack();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.collider.CompareTag("arrow"))
+        {
+            currentHealth -= 25;
+
+            animator.SetBool("isHurt", true);
+            enemyRage = true;
+            StartCoroutine(backToIdle());
+
+            /*Knockback
+            if (rigidEnemy != null)
+            {
+            rigidEnemy.isKinematic = false;
+            Vector2 difference = rigidEnemy.transform.position - transform.position;
+            difference = difference.normalized * impact;
+            rigidEnemy.AddForce(difference, ForceMode2D.Impulse);
+            StartCoroutine(knockStop(rigidEnemy));
+            } */
+
+            if(currentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+        StartCoroutine(enemyDead());
+        this.enabled = false;
+    }
+    private IEnumerator enemyDead()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator backToIdle()
+    {
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("isHurt", false);
+    }
+
+    /* private IEnumerator knockStop(Rigidbody2D rigidEnemy)
+    {
+        if(rigidEnemy != null) {
+        yield return new WaitForSeconds(knockTime);
+        rigidEnemy.velocity = Vector2.zero;
+        rigidEnemy.isKinematic = true;
+        }
+    } */
+
+    public void checkDistance()
+    {
+        if(Vector2.Distance(target.position, transform.position) <= detectRadius && Vector2.Distance(target.position, transform.position) > stopRadius || enemyRage == true)
+        {
+            Vector2 moving = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            rigidEnemy.MovePosition(moving);
+            //X und Y Werte der Bewegung an Animator übermitteln -> Animationen werden aufgerufen
+            animator.SetFloat("horizontal", - (transform.position.x - target.position.x));
+            animator.SetFloat("vertical", - (transform.position.y - target.position.y));
+
+            animator.SetBool("walk", true);
+        }
+
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+    }
+
+    //Enemy Attacks und Player-Schaden Management + Player Tod:
+    public void controlAttack()
+    {
+        if(Vector2.Distance(target.position, transform.position) <= attackRadius && canAttack == true)
+        {
+            StartCoroutine(hitInterval());
+        }
+        else if(playerHealth <= 0)
+        {
+            Debug.Log("Player dead");
+        }
+    }
+
+    public IEnumerator hitInterval()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(0.5f);
+
+        if(Vector2.Distance(target.position, transform.position) <= attackRadius)
+        {
+            playerHealth -= 20;
+            playerAnimator.SetBool("isHurt", true);
+            animator.SetBool("attack", true);
+            enemyRage = false;
+            StartCoroutine(backtoPlayerIdle());
+            StartCoroutine(attackToIdle());
+        }
+        else {
+            canAttack = true;
+        }
+    }
+
+    public IEnumerator backtoPlayerIdle()
+    {
+        yield return new WaitForSeconds(0.4f);
+        playerAnimator.SetBool("isHurt", false);
+        canAttack = true;
+    }
+    public IEnumerator attackToIdle()
+    {
+        yield return new WaitForSeconds(0.2f);
+        animator.SetBool("attack", false);
+    }
+}
